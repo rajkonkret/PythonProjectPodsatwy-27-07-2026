109183ca32bdf683c28a0ca63a45f9573b4a9613
radek <rajkonkret660@gmail.com>
2026-07-27 13:57:57 +0200

    [1 20] update teksty.py

---
+++ day1/formaty.py
@@ -0,0 +1,14 @@
+user = "Tomek"  # str
+wiek = 45  # int
+liczba = 90876545632124678  # int
+
+wersja = 3.9001
+print(type(wersja))  # <class 'float'>, liczba zmiennoprzenkowa
+
+print("Witaj %s, masz teraz %d lat." % (user, wiek))
+# Witaj Tomek, masz teraz 45 lat.
+# %s - str
+# %d - digit
+
+# print("Witaj %d, masz teraz %s lat." % (user, wiek))
+# TypeError: %d format: a real number is required, not str
