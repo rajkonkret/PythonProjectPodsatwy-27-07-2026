6cdf075bbe72c73546c677afa54b6f64acdb93ed
radek <rajkonkret660@gmail.com>
2026-07-27 12:44:14 +0200

    [1 12] update teksty.py

--- day1/teksty.py
+++ day1/teksty.py
@@ -7,6 +7,8 @@ print(type(tekst))
 # teksty są niemutowalne
 tekst.upper()  # Return a copy of the string converted to uppercase.
 print(tekst)  # Witaj Świecie
+
+# wyswietlanie kopii tekstu
 print(tekst.upper())  # WITAJ ŚWIECIE
 tekst_upper = tekst.upper()
 print(tekst_upper)  # WITAJ ŚWIECIE
