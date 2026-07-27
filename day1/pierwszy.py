46bc44c45c59cc419f1aaf9edb0fcb011f7eb687
radek <rajkonkret660@gmail.com>
2026-07-27 11:52:47 +0200

    [1 5] update pierwszy.py

--- day1/pierwszy.py
+++ day1/pierwszy.py
@@ -1,3 +1,22 @@
 # https://peps.python.org/pep-0008/
 # snake_case
 # ctrl alt l - formatowanie
+
+print('Hello World')  # wypisz/wydrukuj
+# (.venv) PS C:\Users\CSComarch\PycharmProjects\PythonProjectPodsatwy-27-07-2026> python .\main.py
+# Hi, PyCharm
+
+print("Witaj Świecie")
+print("radek \"komp\"")  # radek "komp"
+print('radek "komp"')  # radek "komp"
+
+# ctrl / - komentarz
+# print('Radek")
+#   File "C:\Users\CSComarch\PycharmProjects\PythonProjectPodsatwy-27-07-2026\day1\pierwszy.py", line 13
+#     print('Radek")
+#           ^
+# SyntaxError: unterminated string literal (detected at line 13)
+#
+# Process finished with exit code 1 - program zakonczył się z błedem
+
+print("Dalsza częśc programu")
