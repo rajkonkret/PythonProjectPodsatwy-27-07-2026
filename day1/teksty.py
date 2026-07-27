e6442140d0cbb4590f00365e6d32375e6c9cd2ab
radek <rajkonkret660@gmail.com>
2026-07-27 12:41:40 +0200

    [1 11] update pierwszy.py

---
+++ day1/teksty.py
@@ -0,0 +1,13 @@
+tekst = "Witaj Świecie"
+print(tekst)
+print(type(tekst))
+# Witaj Świecie
+# <class 'str'>
+
+# teksty są niemutowalne
+tekst.upper()  # Return a copy of the string converted to uppercase.
+print(tekst)  # Witaj Świecie
+print(tekst.upper())  # WITAJ ŚWIECIE
+tekst_upper = tekst.upper()
+print(tekst_upper)  # WITAJ ŚWIECIE
+print(tekst)  # Witaj Świecie - oryginał sie nie zmieni!
