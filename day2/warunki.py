b9b3c9410e4312c0fdebc6486f01065c4f278d3c
radek <rajkonkret660@gmail.com>
2026-07-28 14:21:17 +0200

    [2 44] update typy_danych_5_slownik.py

--- day2/warunki.py
+++ day2/warunki.py
@@ -72,3 +72,22 @@ if (n := len(a)) > 3:
 # jesli zarobki mniejsze od 10000 podatek 0
 # dla pozostałych podatek 90% (0.9)
 # wypisac obliczony podatek
+
+# dodaj podatek 0.2 dla przedziału 10_000 do 39999
+# dodaj podatek 0.4 dla przedzialu 40000 do 99999
+zarobki = int(input("Podaj zarobki: "))
+podatek = 0
+
+# tylko jeden warunek może być spełniony
+if zarobki < 10_000:
+    podatek = 0
+elif zarobki < 40_000:
+    podatek = 0.2
+elif zarobki < 100_000:
+    podatek = 0.4
+else:
+    podatek = 0.9
+
+print(f"Podatek wynosi: {zarobki * podatek} pln.")
+# Podaj zarobki: 125000
+# Podatek wynosi: 112500.0 pln.
