df6451fec56e8b3abd94cf864531c4684306239e
radek <rajkonkret660@gmail.com>
2026-07-28 10:33:23 +0200

    [2 12] update typy_danych_3_krotka.py

--- day2/typy_danych_3_krotka.py
+++ day2/typy_danych_3_krotka.py
@@ -32,3 +32,26 @@ print(tupla_imiona.index("Radek"))  # indeks 2
 print(tupla_imiona.count("Radek"))  # występuje jeden raz
 
 print(len(tupla_imiona))  # liczba elementów: 4
+
+tup = 1, 2
+print(type(tup))  # <class 'tuple'>
+
+# a - pierwszy, b = drugi
+a = tup[0]
+b = tup[1]
+print(a, b)  # 1 2
+
+# rozpakowanie tupli
+a, b = tup
+print(a, b)  # 1 2
+
+# zamiana miejscami wartości
+a, b = b, a
+print(a, b)  # 2 1
+
+print(tupla_imiona)  # ('Zenek', 'Marek', 'Radek', 'Ania')
+# name1, name2, name3
+# name1, name2, name3 = tupla_imiona # ValueError: too many values to unpack (expected 3, got 4)
+name1, name2, *name3 = tupla_imiona # * dowolna ilosc elementów
+print(name1, name2, name3) # Zenek Marek ['Radek', 'Ania']
+
