7f7ecd82ea6c6e8d5bc7d01a64d3a4d92dd7d5c6
radek <rajkonkret660@gmail.com>
2026-07-28 11:39:35 +0200

    [2 24] update typy_danych_5_slownik.py

---
+++ day2/typy_danych_5_slownik.py
@@ -0,0 +1,14 @@
+# słownik - para klucz : wartosć
+# {'user' : 'Radek'}
+# klucze nie mogą się powtarzać
+# {"firstName":"John", "lastName":"Doe"}
+# słownik jest odpowiednikiem jsona
+
+# pusty słownik
+dictionary = {}
+print(dictionary)  # {}
+print(type(dictionary))  # <class 'dict'>
+
+dict_1 = dict()
+print(dict_1)  # {}
+print(type(dict_1))  # <class 'dict'>
