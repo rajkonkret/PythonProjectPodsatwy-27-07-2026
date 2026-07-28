dbe466da6a3539067e7ad4248b4e03b30239d43c
radek <rajkonkret660@gmail.com>
2026-07-28 11:46:59 +0200

    [2 27] update typy_danych_5_slownik.py

--- day2/typy_danych_5_slownik.py
+++ day2/typy_danych_5_slownik.py
@@ -13,9 +13,26 @@ dict_1 = dict()
 print(dict_1)  # {}
 print(type(dict_1))  # <class 'dict'>
 
-# dodanie elementów do słonika
+# dodanie elementów do słownika
 dictionary['imie'] = "Radek"
 print(dictionary)  # {'imie': 'Radek'}
 
 # dodac klucz wiek
+dictionary['wiek'] = 56
+print(dictionary)  # {'imie': 'Radek', 'wiek': 56}
 
+print(dictionary.keys())  # dict_keys(['imie', 'wiek'])
+print(dictionary.values())  # dict_values(['Radek', 56])
+print(dictionary.items())  # dict_items([('imie', 'Radek'), ('wiek', 56)])
+
+# nadpisanie wartości
+dictionary['imie'] = "Tomek"
+print(dictionary)  # {'imie': 'Tomek', 'wiek': 56}
+
+# wypisanie wartości dla klucza
+print(dictionary['imie'])  # Tomek
+
+dictionary['imie'] = ["Radek", "Tomek", "Magda"]
+print(dictionary)  # {'imie': ['Radek', 'Tomek', 'Magda'], 'wiek': 56}
+
+# wypisac Tomek
