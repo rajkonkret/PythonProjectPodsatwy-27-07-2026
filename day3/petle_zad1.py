71a225f3b28d75c661680250967ff75d23307432
radek <rajkonkret660@gmail.com>
2026-07-29 09:16:23 +0200

    [2 56] update petle_zad1.py

---
+++ day3/petle_zad1.py
@@ -0,0 +1,22 @@
+# pętla - możliwośc wykonania kodu wielokrotnie
+# for - pętla iteracyjna
+
+for i in range(5):  # od 0 do 4
+    print(i)
+
+for i in range(20):
+    pass  # nic nie rób
+
+print(i)  # 19
+
+for i in range(5, 10):
+    print(i)
+# 5
+# 6
+# 7
+# 8
+# 9
+
+for _ in range(10):  # nie ma zmienna
+    print("Test podłoga")
+    print(_)  # 9
