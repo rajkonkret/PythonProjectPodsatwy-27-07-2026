288b2b2b3ea23590c20f6ca77cccb48e0ad44d6c
radek <rajkonkret660@gmail.com>
2026-07-29 14:30:09 +0200

    [3 37] update pliki_zad2.py.py

--- day3/disocunt.py
+++ day3/disocunt.py
@@ -20,3 +20,20 @@ print(type(formated_date))  # <class 'str'>
 # 14:27
 # 14:27:34
 # 2:27 pm
+
+# Dla daty:
+# %Y: Rok z pełną liczbą cyfr, np. "1989", "2023".
+# %y: Rok z dwiema ostatnimi cyframi, np. "89", "23".
+# %m: Miesiąc z zerem wiodącym, np. "01" do "12".
+# %d: Dzień miesiąca z zerem wiodącym, np. "01" do "31".
+# %B: Pełna nazwa miesiąca, np. "January", "December".
+# %b: Skrócona nazwa miesiąca, np. "Jan", "Dec".
+# %A: Pełna nazwa dnia tygodnia, np. "Monday", "Sunday".
+# %a: Skrócona nazwa dnia tygodnia, np. "Mon", "Sun".
+# Dla czasu:
+# %H: Godzina w formacie 24-godzinnym z zerem wiodącym, np. "00" do "23".
+# %I: Godzina w formacie 12-godzinnym z zerem wiodącym, np. "01" do "12".
+# %p: AM/PM.
+# %M: Minuty z zerem wiodącym, np. "00" do "59".
+# %S: Sekundy z zerem wiodącym, np. "00" do "59".
+# %f: Mikrosekundy, np. "000000" do "999999".
