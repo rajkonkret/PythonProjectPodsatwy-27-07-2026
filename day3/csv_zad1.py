228e15ddc0d2b745b33850af4aadf9f8b08c73bd
radek <rajkonkret660@gmail.com>
2026-07-29 15:20:58 +0200

    [3 44] update discount.py

---
+++ day3/csv_zad1.py
@@ -0,0 +1,15 @@
+# Kowalski,Jan,Kłodzko
+# Nowak,Zenon,Szczecin
+# Brzęczyszczykiewicz,Grzegorz,Chrząszczyżewoszyce
+
+# csv - dane oddzilone znakiem podziału ,;tab|
+
+import csv
+
+row = ['radek', 'coe', "3", 0]
+
+filename = 'records.csv'
+
+with open(filename, "w") as csv_f:
+    csv_writer = csv.writer(csv_f)
+    csv_writer.writerow(row)
