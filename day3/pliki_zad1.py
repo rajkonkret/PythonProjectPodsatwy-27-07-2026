812c492c37919f84be3842b14bac81b97a50fbd8
radek <rajkonkret660@gmail.com>
2026-07-29 13:41:19 +0200

    [3 27] update kalkulator.py

---
+++ day3/pliki_zad1.py
@@ -0,0 +1,11 @@
+# działania z plikami
+# filehandler
+
+# open()
+
+# context manager
+# with - context manager w pythonie
+
+with open("test.log", "w") as file:
+    file.write("Powitanie\n")
+    file.write("Jeszcze jedno\n")
