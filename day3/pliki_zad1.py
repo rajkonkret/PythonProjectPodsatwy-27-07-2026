e82fc2161329b3f1b7901545272e9551a3b15a81
radek <rajkonkret660@gmail.com>
2026-07-29 13:45:38 +0200

    [3 28] update kalkulator.py

--- day3/pliki_zad1.py
+++ day3/pliki_zad1.py
@@ -9,3 +9,27 @@
 with open("test.log", "w") as file:
     file.write("Powitanie\n")
     file.write("Jeszcze jedno\n")
+
+# file.write("") # ValueError: I/O operation on closed file.
+
+# x tworzy nowy plik gdy plik nie istnieje!!!
+# FileExistsError: [Errno 17] File exists: 'test.log'
+# with open("test.log", "x") as file:
+#     file.write("Powitanie\n")
+#     file.write("Jeszcze jedno\n")
+
+# a - append - dołacza dane na końcu
+with open("test.log", "a") as file:
+    file.write("Dodane\n")
+    file.write("Dodane\n")
+    file.write("Jeszcze jedno\n")
+
+with open("test.log", "r") as f:
+    lines = f.read()
+
+print(lines)
+# Powitanie
+# Jeszcze jedno
+# Dodane
+# Dodane
+# Jeszcze jedno
