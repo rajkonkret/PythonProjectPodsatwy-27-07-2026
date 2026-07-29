ab867fb186a3c6772676388d3a840403d3d8a2d2
radek <rajkonkret660@gmail.com>
2026-07-29 15:23:08 +0200

    [3 45] update discount.py

--- day3/csv_zad1.py
+++ day3/csv_zad1.py
@@ -7,9 +7,12 @@
 import csv
 
 row = ['radek', 'coe', "3", 0]
+columns = ['name', 'branch', 'year', 'cgpa']
 
 filename = 'records.csv'
 
-with open(filename, "w") as csv_f:
+# newline="" obejscie problemu pustych linii
+with open(filename, "w", newline="") as csv_f:
     csv_writer = csv.writer(csv_f)
+    csv_writer.writerow(columns)
     csv_writer.writerow(row)
