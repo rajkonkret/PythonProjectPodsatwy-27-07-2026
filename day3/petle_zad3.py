682c392d2219b3cbef6767985843d2743af61ac7
radek <rajkonkret660@gmail.com>
2026-07-29 11:31:04 +0200

    [3 18] update petle_zad3.py

--- day3/petle_zad3.py
+++ day3/petle_zad3.py
@@ -9,4 +9,12 @@ while True:
     licznik += 1
     print("Komunikat 2 !!")
     if licznik > 10:
-        break # przerywa pętle
+        break  # przerywa pętle
+
+print(licznik)  # 11
+print(50 * "-")
+
+licznik = 0
+while licznik < 10:
+    licznik += 1
+    print("Komunikt 3 !!!")
