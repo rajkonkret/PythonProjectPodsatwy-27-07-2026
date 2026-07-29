5d08b7ec5736048b038182412a9597444f458857
radek <rajkonkret660@gmail.com>
2026-07-29 15:27:54 +0200

    [3 46] update discount.py

--- day3/csv_zad1.py
+++ day3/csv_zad1.py
@@ -16,3 +16,16 @@ with open(filename, "w", newline="") as csv_f:
     csv_writer = csv.writer(csv_f)
     csv_writer.writerow(columns)
     csv_writer.writerow(row)
+
+dict_name = dict(zip(columns, row))
+print(dict_name)
+print(type(dict_name))
+# {'name': 'radek', 'branch': 'coe', 'year': '3', 'cgpa': 0}
+# <class 'dict'>
+
+filename = 'records_dict.csv'
+
+with open(filename, "w", newline="") as csv_f:
+    csv_writer = csv.DictWriter(csv_f, fieldnames=columns)
+    csv_writer.writeheader()
+    csv_writer.writerow(dict_name)
