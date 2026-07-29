746fdfd03683570702f9181fa435813a11867c33
radek <rajkonkret660@gmail.com>
2026-07-29 14:02:49 +0200

    [3 31] update pliki_zad2.py.py

--- day3/pliki_zad2.py
+++ day3/pliki_zad2.py
@@ -1,4 +1,20 @@
-with open("test.log", "r") as f:
-    lines = f.read()
+# with open("test.log", "r") as f:
+#     lines = f.read()
+#
+# print(lines)
+# Powitanie
+# Jeszcze jedno
+# Dodane
+# Dodane
+# DĹ›odane
+# Jeszcze jedno
 
-print(lines)
+import chardet
+# pip install chardet
+
+# odczyt binarny - rb
+with open('test.log', "rb") as fh:
+    raw_data = fh.read()
+
+print(raw_data)
+# b'Powitanie\r\nJeszcze jedno\r\nDodane\r\nDodane\r\nD\xc5\x9bodane\r\nJeszcze jedno\r\n'
