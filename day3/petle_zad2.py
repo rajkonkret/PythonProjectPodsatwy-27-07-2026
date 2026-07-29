04eec65c795ae38283564882b9933ffc2bf6e0cc
radek <rajkonkret660@gmail.com>
2026-07-29 10:56:00 +0200

    [3 14] update petle_zad1.py

---
+++ day3/petle_zad2.py
@@ -0,0 +1,45 @@
+dictionary = {'imie': "Radek", 'nazwisko': "Kowalski"}
+
+# klucz, wartość, pary
+
+# wypisuje klucze
+for i in dictionary:
+    print(i)
+# imie
+# nazwisko
+
+for i in dictionary.keys():
+    print(i)
+
+# wypisanie wartości
+for i in dictionary.values():
+    print(i)
+# Radek
+# Kowalski
+
+# wypisze pary
+for i in dictionary.items():
+    print(i)
+# ('imie', 'Radek')
+# ('nazwisko', 'Kowalski')
+
+for k, v in dictionary.items():
+    print(k, "<==>", v)
+# imie <==> Radek
+# nazwisko <==> Kowalski
+
+# sep
+# string inserted between values, default a space.
+# end
+# string appended after the last value, default a newline.
+
+for k, v in dictionary.items():
+    print(k, v, sep="<=>")
+# imie<=>Radek
+# nazwisko<=>Kowalski
+
+for k, v in dictionary.items():
+    print(k, v, sep="<=>", end=" | ")
+# imie<=>Radek | nazwisko<=>Kowalski |
+
+print("Radek")
