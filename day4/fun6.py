ea6cc6ee445a7d111f875b9b7fb7fedee37e8305
radek <rajkonkret660@gmail.com>
2026-07-30 12:08:59 +0200

    [4 17] update fun2_zwraca_wynik.py

--- day4/fun6.py
+++ day4/fun6.py
@@ -1,20 +1,27 @@
 # napisać funkcję obliczającą średnią
+# statistics - funkcje staystyczne np.: Średnia (mean)
+import statistics
 
-def srednia(*cyfry):  # dowolna ilośc elemntów przekazanych po pozycji
+
+def srednia(name=None, *cyfry):  # dowolna ilośc elementów przekazanych po pozycji
     print(cyfry)
 
     count = len(cyfry)
 
     suma = 0
+    suma_p = sum(cyfry)
     try:
         for c in cyfry:
             suma += c
 
         avg = suma / count
+        avg_p = suma_p / count
     except Exception as e:
         print("Bład:", e)
     else:
-        print(f"Srednia ocen: {avg}")
+        print(f"Srednia dla ucznia {name} wynosi: {avg}")
+        print(f"Srednia dla ucznia {name} wynosi: {avg_p}")
+        print(f"Srednia dla ucznia {name} wynosi:{statistics.mean(cyfry)}")
     finally:
         print("Następny uczeń")
 
@@ -22,3 +29,6 @@ def srednia(*cyfry):  # dowolna ilośc elemntów przekazanych po pozycji
 srednia()
 srednia(1)
 srednia(1, 2)
+
+srednia("Radek", 6, 7, 8, 6, 7, 8, 8, 6)
+name, *cyfra = "Radek", 6, 7, 8, 6, 7, 8, 8, 6
