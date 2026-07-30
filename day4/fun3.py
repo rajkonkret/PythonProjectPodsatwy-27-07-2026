79c41516981774163b8b52a69039d655c6ec1ce5
radek <rajkonkret660@gmail.com>
2026-07-30 10:35:33 +0200

    [4 9] update fun2_zwraca_wynik.py

--- day4/fun3.py
+++ day4/fun3.py
@@ -3,10 +3,37 @@ b = 10
 
 
 def dodaj():
-    a = 7
+    a = 7  # zmienne lokalne, widoczne tylko wewnątrz funkcji
     b = 8
     print(a + b)
 
 
-print(f"Zmienna a z góry(globalna) {a}")
-dodaj()
+def dodaj2():
+    print(a + b)  # dziła na globalnych
+
+
+def dodaj3():
+    global a
+    a = 7  # zmieniamy zawartość globalnej a
+    b = 90
+    print(a + b)
+
+
+def dodaj4():
+    a = 10
+    b = 9
+    c = a + b  # lokalne
+    print(c)
+
+
+print(f"Zmienna a z góry(globalna) {a}")  # Zmienna a z góry(globalna) 10
+dodaj()  # 15
+print(f"Zmienna a z góry(globalna) {a}")  # Zmienna a z góry(globalna) 10
+dodaj2()  # 20
+print(f"Zmienna a z góry(globalna) {a}")  # Zmienna a z góry(globalna) 10
+dodaj3()  # 97
+print(f"Zmienna a z góry(globalna) {a}")  # Zmienna a z góry(globalna) 7
+dodaj2()  # 17
+print(f"Zmienna a z góry(globalna) {a}")  # Zmienna a z góry(globalna) 7
+dodaj4()  # 19
+# print(c)  # NameError: name 'c' is not defined
