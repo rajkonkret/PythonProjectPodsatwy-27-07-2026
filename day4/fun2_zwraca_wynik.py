c809fbddb4c2655d0a89a413830cf4b89b8e4cff
radek <rajkonkret660@gmail.com>
2026-07-30 10:08:59 +0200

    [4 5] update json_zad1.py.py

---
+++ day4/fun2_zwraca_wynik.py
@@ -0,0 +1,17 @@
+# funkcje zwracające wynik
+# kończy się słówkiem return
+
+a = 9
+b = 98
+
+
+def dodaj():
+    return a + b  # zwróć wynik a + b
+
+
+# funkcja odejmij, z trzema argumentami domyslnymi
+
+print(dodaj())  # 107
+
+wynik = dodaj()
+print("Wynik:", wynik)  # Wynik: 107
