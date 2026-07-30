0c56ea064936701e8158376e5af4ce17ce162f48
radek <rajkonkret660@gmail.com>
2026-07-30 11:20:13 +0200

    [4 14] update fun2_zwraca_wynik.py

--- day4/fun5_kantor.py
+++ day4/fun5_kantor.py
@@ -3,3 +3,16 @@
 # w zależności od parametru(kantor) (if) zwróci jedną z funkcji (adres)
 # przekazanie kwoty do funkcji usd, eur
 
+def kantor(waluta):
+    print("Otwieram kantor")
+
+    def usd():
+        pass
+
+    def eur():
+        pass
+
+    if waluta == "eur":
+        return eur  # zwracamy adres
+    else:
+        return usd
