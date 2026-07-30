85ba69b28e275058f0880c924ff0e49ab6418174
radek <rajkonkret660@gmail.com>
2026-07-30 15:09:37 +0200

    [4 37] update kl1

--- day4/kl1.py
+++ day4/kl1.py
@@ -12,6 +12,10 @@ class Human:
     # pass
     """Klasa Human opisująca człowieka w pythonie"""
 
+    imie = ""
+    wiek = None
+    plec = "k"
+
 
 cz1 = Human()
 print(Human.__doc__)  # Klasa Human opisująca człowieka w pythonie
@@ -20,4 +24,13 @@ print(Human.__doc__)  # Klasa Human opisująca człowieka w pythonie
 # pydoc -b - serwer dokumentacji
 # pydoc -w kl1.py - plik html z dokumentacją
 
+print(cz1.imie)
+cz1.imie = "Anna"
+cz1.wiek = 34
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+# Anna
+# 34
+# k
 
