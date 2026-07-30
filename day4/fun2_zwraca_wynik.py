4b3cd4dc5c2cae847030b5097543c3789174b9d7
radek <rajkonkret660@gmail.com>
2026-07-30 10:17:40 +0200

    [4 6] update fun2_zwraca_wynik.py

--- day4/fun2_zwraca_wynik.py
+++ day4/fun2_zwraca_wynik.py
@@ -10,8 +10,29 @@ def dodaj():
 
 
 # funkcja odejmij, z trzema argumentami domyslnymi
+def odejmij(a=0, b=0, c=0):
+    return a - b - c
+
 
 print(dodaj())  # 107
 
 wynik = dodaj()
 print("Wynik:", wynik)  # Wynik: 107
+
+print(odejmij(1, 2, 3))  # -4
+print(odejmij(1, 2, c=3))  # -4
+print(odejmij(1, b=2, c=3))  # -4
+print(odejmij())  # 0
+print(odejmij(a, b))  # -89
+
+
+def oblicz_vat(kwota, vat=23):
+    return kwota * (100 + vat) / 100
+
+
+print(oblicz_vat(1000))
+print(oblicz_vat(1000, 8))
+print(oblicz_vat(vat=15, kwota=1000))
+# 1230.0
+# 1080.0
+# 1150.0
