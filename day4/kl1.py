24dd4ffffa3e7d35e07135a52545d9416e5dc0cd
radek <rajkonkret660@gmail.com>
2026-07-30 14:46:19 +0200

    [4 34] update kl1

---
+++ day4/kl1.py
@@ -0,0 +1,10 @@
+# klasa - przepis, szablon
+# cechy (zmienne)
+# metody (funkcje)
+# obiekt - instancja klasy
+# klasa musi zostac najpierw zadeklarowany
+# tworzenie obiektu kalsy uruchamia metodę inicjalizującą (konstruktor) __init__
+# __del__ - destruktor
+# paradygmanty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+
