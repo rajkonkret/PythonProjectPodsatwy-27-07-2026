89eac942e4fd51b5a670d35a64be5d224ff58c49
radek <rajkonkret660@gmail.com>
2026-07-31 12:29:07 +0200

    [5 26] update api_chuck.py.py

---
+++ day5/api_chuck.py
@@ -0,0 +1,7 @@
+# rest api
+# GET: Pobiera dane z serwera (np. listę produktów).
+# POST: Tworzy nowy zasób na serwerze (np. dodaje nowego użytkownika).
+# PUT / PATCH: Aktualizuje istniejące dane (PUT zastępuje całość, PATCH tylko wskazany fragment)
+# DELETE: Usuwa wskazany zasób z serwera.
+
+url = "https://api.chucknorris.io/jokes/random"
