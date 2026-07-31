8b2348f8048407775f724afc74770b23cc24b3d7
radek <rajkonkret660@gmail.com>
2026-07-31 14:13:05 +0200

    [5 40] update baza3_orm.py

---
+++ day5/baza3_orm.py
@@ -0,0 +1,24 @@
+# Mapowanie obiektowo-relacyjne (ang. Object-Relational Mapping – ORM)
+# – sposób odwzorowania obiektowej architektury systemu informatycznego na bazę danych
+# (lub inny element systemu) o relacyjnym charakterze.
+
+# sqlalchemy, pewee
+# django orm
+
+# pip install sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+
+
+engine = create_engine('sqlite:///my_database.db', echo=True)
+
+Base.metadata.create_all(engine)
