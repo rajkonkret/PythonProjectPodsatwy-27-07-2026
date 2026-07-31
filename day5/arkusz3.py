e6c5a151922978fb8759b81cfc88b16b870ce7f4
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2026-07-31 12:15:29 +0200

    Update arkusz3.py

--- day5/arkusz3.py
+++ day5/arkusz3.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import numpy as np
+
+technologies = ['Spark', 'Pandas', "Python", 'PHP']
+fee = [25000, 20000, 15000, 18000, 22000]
+duration = ['50 Days', '30 Days', np.nan, '15 Days']
+discount = [2000, 1500, 800, 500, 500]
+columns = ['Courses', 'Fee', 'Duration', 'Discount']
