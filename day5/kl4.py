848c914635d81c84b9c64119021e153227dfbd64
radek <rajkonkret660@gmail.com>
2026-07-31 09:44:21 +0200

    [5 5] update kl4

--- day5/kl4.py
+++ day5/kl4.py
@@ -13,6 +13,13 @@ class Ptak:
         self.gatunek = gatunek
         self.szybkosc = szybkosc
 
+    def latam(self):
+        print("Tu", self.gatunek, "Lecę z szybkością:", self.szybkosc, "km/h")
+
+    def __str__(self) -> str:
+        return f"{self.gatunek} {self.szybkosc}"
+
 
 or1 = Ptak("Orzeł", 50)
-print(or1)  # <__main__.Ptak object at 0x0000024BDF5D9A90> -> __str__
+print(or1)  # <__main__.Ptak object at 0x0000024BDF5D9A90> -> __str__ ->   Orzeł 50
+or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
