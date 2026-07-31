d51ecda21e9531866659720ede50bf29867fdef5
radek <rajkonkret660@gmail.com>
2026-07-31 09:36:16 +0200

    [5 2] update kl4

---
+++ day5/kl4.py
@@ -0,0 +1,14 @@
+class Ptak:
+    """
+    Klasa Ptak opisująca ptaka w Pythonie
+    """
+
+    def __init__(self, gatunek, szybkosc):
+        """
+        Metoda inicjalizująca - konstruktor
+        :param gatunek:
+        :param szybkosc:
+        """
+
+        self.gatunek = gatunek
+        self.szybkosc = szybkosc
