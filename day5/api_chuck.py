3668d35edf1bbf3f5905bf8902517c5a4c7ce681
radek <rajkonkret660@gmail.com>
2026-07-31 12:35:06 +0200

    [5 27] update api_chuck.py.py

--- day5/api_chuck.py
+++ day5/api_chuck.py
@@ -4,4 +4,16 @@
 # PUT / PATCH: Aktualizuje istniejące dane (PUT zastępuje całość, PATCH tylko wskazany fragment)
 # DELETE: Usuwa wskazany zasób z serwera.
 
+# klient http httpx, niquests
+import requests
+
+# pip install requests
+
 url = "https://api.chucknorris.io/jokes/random"
+
+response = requests.get(url)
+print(response)  # <Response [200]>
+# 2xx ok
+# 3xx warningi, przekierowanie
+# 4xx 404 - brak strony, 400 Bad request
+# 5xx błedy po stronie serwera
