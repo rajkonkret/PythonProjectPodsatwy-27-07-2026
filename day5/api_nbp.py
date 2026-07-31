492a0f1082e115784f8b98cea0d033bc7e76359f
radek <rajkonkret660@gmail.com>
2026-07-31 12:56:12 +0200

    [5 30] update api_chuck.py.py

--- day5/api_nbp.py
+++ day5/api_nbp.py
@@ -1,3 +1,19 @@
 url = "https://api.nbp.pl/api/exchangerates/rates/A/eur/"
 
 # nazwe walute, kurs waluty
+import requests
+
+response = requests.get(url)
+data = response.json()
+
+print(data)
+print(type(data))
+# {'table': 'A', 'currency': 'euro', 'code': 'EUR',
+# 'rates': [{'no': '147/A/NBP/2026', 'effectiveDate': '2026-07-31', 'mid': 4.3128}]}
+# <class 'dict'>
+
+print('Waluta:', data['currency'])  # Waluta: euro
+print("Kurs:", data['rates'])  # [{'no': '147/A/NBP/2026', 'effectiveDate': '2026-07-31', 'mid': 4.3128}]
+print("Kurs:", data['rates'][0])  # {'no': '147/A/NBP/2026', 'effectiveDate': '2026-07-31', 'mid': 4.3128}
+print("Kurs:", data['rates'][0]['mid'])  # Kurs: 4.3128
+print("Kurs:", type(data['rates'][0]['mid']))  # Kurs: <class 'float'>
