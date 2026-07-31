d901ff3a85657f4c4e0456354aced5e9b7ce93d0
radek <rajkonkret660@gmail.com>
2026-07-31 09:58:54 +0200

    [5 7] update kl4

--- day5/kl4.py
+++ day5/kl4.py
@@ -20,6 +20,18 @@ class Ptak:
         return f"{self.gatunek} {self.szybkosc}"
 
 
+class Kura(Ptak):
+    """
+    Klasa Kura, dziedziczy po kalsie Ptak
+    """
+
+    def __init__(self, gatunek):
+        super().__init__(gatunek, 0)  # musimy wywołąc super(), super() - klasa nadrzędna
+
+    def latam(self):
+        print("Tu", self.gatunek, "Ja nie latam")
+
+
 or1 = Ptak("Orzeł", 50)
 print(or1)  # <__main__.Ptak object at 0x0000024BDF5D9A90> -> __str__ ->   Orzeł 50
 or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
@@ -27,3 +39,7 @@ or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
 kur1 = Ptak("Kura", 0)
 print(kur1)  # Kura 0
 kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
+
+kur2 = Kura("Kura zielonóżka")
+print(kur2)  # Kura zielonóżka 0
+kur2.latam()  # Tu Kura zielonóżka Ja nie latam
