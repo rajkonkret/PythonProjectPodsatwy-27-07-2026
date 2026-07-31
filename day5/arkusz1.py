b6356df732ab94c5113d1bf175c90b8ce3504341
radek <rajkonkret660@gmail.com>
2026-07-31 12:02:06 +0200

    [5 20] update arkusz1.py

--- day5/arkusz1.py
+++ day5/arkusz1.py
@@ -26,3 +26,11 @@ for i in range(worksheet.max_row):
         lista.append(col[i].value)
 
 print(lista)
+
+for i in range(0, len(lista), 3):
+    print(lista[i: i + 3])  # slicowanie
+# ['Sales Date', 'Sales Person', 'Amount']
+# [datetime.datetime(2018, 5, 12, 0, 0), 'Sila Ahmed', 60000]
+# [datetime.datetime(2019, 12, 6, 0, 0), 'Mir Hossain', 50000]
+# [datetime.datetime(2020, 8, 9, 0, 0), 'Sarmin Jahan', 45000]
+# [datetime.datetime(2021, 4, 7, 0, 0), 'Mahmudul Hasan', 30000] # f"{i<10}"
