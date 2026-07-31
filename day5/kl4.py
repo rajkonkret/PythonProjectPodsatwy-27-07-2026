cb86f85fc3e84a14df6a846bb709c85cf37e47ed
radek <rajkonkret660@gmail.com>
2026-07-31 10:14:08 +0200

    [5 10] update kl4

--- day5/kl4.py
+++ day5/kl4.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 # klasa abstrakcyjna - klasa z której nie można tworzyc obiektów
@@ -20,6 +20,10 @@ class Ptak(ABC):
     def latam(self):
         print("Tu", self.gatunek, "Lecę z szybkością:", self.szybkosc, "km/h")
 
+    @abstractmethod  # metoda abstrakcyjna
+    def wydaj_odglos(self):
+        pass
+
     def __str__(self) -> str:
         return f"{self.gatunek} {self.szybkosc}"
 
@@ -35,20 +39,28 @@ class Kura(Ptak):
     def latam(self):
         print("Tu", self.gatunek, "Ja nie latam")
 
+    def wydaj_odglos(self):
+        print("Ko ko ko ko ko ko")
+
 
 class Orzel(Ptak):
     """
     Klasa Orzeł dziedziczy po klasie Ptak
     """
 
+    def wydaj_odglos(self):
+        print("Kier kiir kier kir kir")
 
-or1 = Ptak("Orzeł", 50)
-print(or1)  # <__main__.Ptak object at 0x0000024BDF5D9A90> -> __str__ ->   Orzeł 50
-or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
 
-kur1 = Ptak("Kura", 0)
-print(kur1)  # Kura 0
-kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
+# nie można tworzyć obiektów klasy abstrakcyjnej
+# TypeError: Can't instantiate abstract class Ptak without an implementation for abstract method 'wydaj_odglos'
+# or1 = Ptak("Orzeł", 50)
+# print(or1)  # <__main__.Ptak object at 0x0000024BDF5D9A90> -> __str__ ->   Orzeł 50
+# or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
+#
+# kur1 = Ptak("Kura", 0)
+# print(kur1)  # Kura 0
+# kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
 
 kur2 = Kura("Kura zielonóżka")
 print(kur2)  # Kura zielonóżka 0
