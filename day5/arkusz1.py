b1520253faf26653c34dc39e66e5e7b4fd81dac4
radek <rajkonkret660@gmail.com>
2026-07-31 11:59:47 +0200

    [5 19] update arkusz1.py

--- day5/arkusz1.py
+++ day5/arkusz1.py
@@ -9,3 +9,20 @@ workbook = openpyxl.load_workbook('sales.xlsx')
 worksheet = workbook.active
 
 print(worksheet)  # <Worksheet "Arkusz1">
+
+print(worksheet.title)  # Arkusz1
+
+lista = []
+for i in worksheet:
+    print(i)
+# (<Cell 'Arkusz1'.A1>, <Cell 'Arkusz1'.B1>, <Cell 'Arkusz1'.C1>)
+# (<Cell 'Arkusz1'.A2>, <Cell 'Arkusz1'.B2>, <Cell 'Arkusz1'.C2>)
+# (<Cell 'Arkusz1'.A3>, <Cell 'Arkusz1'.B3>, <Cell 'Arkusz1'.C3>)
+# (<Cell 'Arkusz1'.A4>, <Cell 'Arkusz1'.B4>, <Cell 'Arkusz1'.C4>)
+# (<Cell 'Arkusz1'.A5>, <Cell 'Arkusz1'.B5>, <Cell 'Arkusz1'.C5>)
+
+for i in range(worksheet.max_row):
+    for col in worksheet.iter_cols(1, worksheet.max_column):
+        lista.append(col[i].value)
+
+print(lista)
